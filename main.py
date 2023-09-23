821ca6ce79871591f42f8f1cfeaa5132d8f9574b
Isaac Kittrell <i_k35@txstate.edu>
2023-09-22 21:30:55 -0500

    initial commit (fonts demo)

---
+++ main.py
@@ -0,0 +1,55 @@
+import pygame
+import random
+
+pygame.init()
+clock = pygame.time.Clock()
+
+window = pygame.display.set_mode((1200,700))
+screen_w = pygame.display.Info().current_w
+screen_h = pygame.display.Info().current_h
+pygame.display.set_caption("2D Shooter")
+
+# define fonts
+font = pygame.font.SysFont("vladimirscript", 30)
+for x in pygame.font.get_fonts():
+    print(x)
+
+# define colors
+text_color = (255, 255, 255)
+
+def draw_text(text, font, text_color, x, y):
+    img = font.render(text, True, text_color)
+    window.blit(img, (x,y))
+
+def rand_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return (r,g,b)
+
+run = True
+while run:
+    i, j = 0, 0
+    for font_name in pygame.font.get_fonts():
+        font = pygame.font.SysFont(font_name, 20)
+        draw_text(font_name, font, text_color, i, j)
+        j += 15
+        if j >= screen_h - 15:
+            j = 0
+            i += 190
+    #draw_text("cool game", font, text_color, 0, 0)
+    #draw_text("cool game", font, text_color, 0, 15)
+    #draw_text("cool game", font, text_color, 120, 0)
+    # draw_text(str(random.randint(0,250)), font, text_color, random.randint(0,screen_w), random.randint(0,screen_h))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
